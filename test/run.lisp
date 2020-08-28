1233d9581bcff760065cde4c8fd972d226fea26f
Steve Losh <steve@stevelosh.com>
2020-08-28 19:21:53 -0400

    Travis pls

--- test/run.lisp
+++ test/run.lisp
@@ -3,10 +3,6 @@
 (when (uiop:getenv "CI")
   #+sbcl (sb-ext:disable-debugger))
 
-(format t "meow 1")
-(ql:quickload 'cl-digraph :verbose t)
-(format t "wow 2")
 (ql:quickload 'cl-digraph.test :verbose t)
-(format t "hello 3")
 (time (asdf:test-system 'cl-digraph))
 (uiop:quit)
