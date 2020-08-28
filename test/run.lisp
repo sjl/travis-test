ca57b2866175464e7e0a4c9df93ae56f786d2398
Steve Losh <steve@stevelosh.com>
2020-08-28 19:10:38 -0400

    Travis please

--- test/run.lisp
+++ test/run.lisp
@@ -1,6 +1,6 @@
 #+ecl (setf compiler:*user-cc-flags* "-Wno-shift-negative-value")
 
-(when (string= "t" (uiop:env "CI"))
+(when (uiop:getenv "CI")
   #+sbcl (sb-ext:disable-debugger))
 
 (ql:quickload 'cl-digraph :verbose t)
