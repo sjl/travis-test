23fd97b2692ca2d204faeaa3c4ea75e1feb8d453
Steve Losh <steve@stevelosh.com>
2020-08-28 19:41:54 -0400

    sigh

--- test/run.lisp
+++ test/run.lisp
@@ -1,12 +1,5 @@
 #+ecl (setf compiler:*user-cc-flags* "-Wno-shift-negative-value")
 
-;; (when (uiop:getenv "CI")
-;;   (setf *debugger-hook*
-;;         (lambda (e dh)
-;;           (declare (ignore dh))
-;;           (format *error-output* "~2%FAILED:~2%~A~2%" e)
-;;           (uiop:quit 1))))
-
 (ql:quickload 'cl-digraph.test)
 (time (asdf:test-system 'cl-digraph))
 (uiop:quit 0)
