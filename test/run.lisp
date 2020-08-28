57710d61faa64cf88b9a5a83134f04a33f05a1ab
Steve Losh <steve@stevelosh.com>
2020-08-28 19:28:50 -0400

    borkdf

--- test/run.lisp
+++ test/run.lisp
@@ -2,7 +2,11 @@
 
 (when (uiop:getenv "CI")
   #+sbcl (sb-ext:disable-debugger))
+(setf *debugger-hook*
+      (lambda (e dh)
+        (format t "~A" e)
+        (uiop:quit 1)))
 
 (ql:quickload 'cl-digraph.test :verbose t)
 (time (asdf:test-system 'cl-digraph))
-(uiop:quit)
+(uiop:quit 0)
